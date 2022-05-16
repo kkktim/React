d2b1eaec03d72ba023de4ee5d928ee2da58fef35
unknown <rkdxogh1987@gmail.com>
2022-05-16 15:48:37 +0900

    2022/05/16 ch06 실습

---
+++ ch06/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="Welcome" style={{ backgroundColor: "green" }}>
+      <h4>Hello</h4>
+      <p>Welcome 컴포넌트 입니다.</p>
+    </div>
+  );
+};
+
+export default Welcome;
